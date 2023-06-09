51f8d251c564d978aa91da070170e16dfd499088
MishaITSTEP <Krav_hp83@student.itstep.org>
2023-06-09 20:59:34 +0300

    3 project

---
+++ 03_Binding/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _03_Binding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow:Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+    [PropertyChanged.AddINotifyPropertyChangedInterface]
+    public class ViewModel
+    {
+        public int ColorRed{get; set; }
+        public int ColorGreen{get; set; }
+        public int ColorBlue{get; set;}
+
+
+    }
+}
