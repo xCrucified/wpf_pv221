a7962dc4a29da6e5611018db7f588bf0715aaa74
Максим <@DESKTOP-VHT1GOT>
2023-06-10 21:15:45 +0300

    Fixed colors changed

--- 03_Binding/MainWindow.xaml.cs
+++ 03_Binding/MainWindow.xaml.cs
@@ -1,9 +1,10 @@
-﻿using PropertyChanged;
-using System;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -21,24 +22,92 @@ namespace _03_Binding
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+
     public partial class MainWindow:Window
     {
+        
         public MainWindow()
         {
             InitializeComponent();
             ViewModel viewModel = new ViewModel();
-            DataContext = viewModel;
+            this.DataContext = viewModel;
+        }
+
+        private void deleteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void addBtn_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
-    
-    [PropertyChanged.AddINotifyPropertyChangedInterface]
-    public class ViewModel
+
+    public class ViewModel : INotifyPropertyChanged
     {
-        public byte Alpha { get; set; }
-        public byte Red { get; set; }
-        public byte Green { get; set; }
-        public byte Blue { get; set; }
+        private ObservableCollection<Color> listColor = new();
+
+        private byte alpha;
+        private byte red;
+        private byte green;
+        private byte blue;
+
+        public byte Alpha
+        {
+            get { return alpha; }
+            set
+            {
+                alpha = value;
+                OnPropertyChanged(nameof(CurrentColor));
+            }
+        }
+        public byte Red
+        {
+            get { return red; }
+            set
+            {
+                red = value;
+                OnPropertyChanged(nameof(CurrentColor));
+            }
+        }
+        public byte Green
+        {
+            get { return green; }
+            set
+            {
+                green = value;
+                OnPropertyChanged(nameof(CurrentColor));
+            }
+        }
+        public byte Blue
+        {
+            get { return blue; }
+            set
+            {
+                blue = value;
+                OnPropertyChanged(nameof(CurrentColor));
+            }
+        }
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
         public Color CurrentColor => Color.FromArgb(Alpha, Red, Green, Blue);
+        private void addClick()
+        {
+            Color color = CurrentColor; // Приклад кольору (червоний)
+
+            // Конвертація в шістнадцятковий код з використанням великих літер (наприклад, "#FF0000")
+            color.GetHashCode();
+
+            // Конвертація в шістнадцятковий код з використанням малих літер (наприклад, "#ff0000")
+            
+        }
+ 
 
     }
 
